e89a1d33833c4bacdfefe12311f623642bd1a9d9
Kyle Morton <kylemortondevelopment@gmail.com>
2018-01-10 22:56:21 -0600

    tic tac toe working, before move history

--- src/index.js
+++ src/index.js
@@ -2,29 +2,72 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null 
-        };
-    }
-    render() {
-      return (
-        <button className="square" onClick={()=> this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
-    }
+// class Square extends React.Component {
+//     // *need constructor only if component has state
+//     // constructor(props){ 
+//     //     super(props);
+//     //     this.state = {
+//     //         value: null 
+//     //     };
+//     // }
+//     render() {
+//       return (
+//         <button className="square" onClick={() => this.props.onClick()}>
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+
+  /* Functional Component -> needs nothing more than render so use shorthand */
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true
+      }
+    }
+    handleClick(i) {
+
+      /* REM: making a copy, updating, then reassigning is FASTER than mutating existing state value */
+      /* REM: tracking changes to mutable object is complex, tracking changes to immutable (new copy) is MUCH easier */
+
+      const squares = this.state.squares.slice(); //get copy of squares instead of changing existing
+
+      //if someone won or square already has value
+      if (calculateWinner(squares) || squares[i])
+        return;
+      
+      squares[i] = this.state.xIsNext ? 'X' : 'O'; //update chosen square with x
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      }); //set squares in state
+    }
     renderSquare(i) {
-      return <Square value={i}/>;
+      return <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />;
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -63,6 +106,26 @@ class Square extends React.Component {
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
